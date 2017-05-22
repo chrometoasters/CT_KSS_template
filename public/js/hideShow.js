1b1174e7d9fe7f42f55bbf6c8ff6ecc615996729
Bas van Lunteren <bas.van.lunteren@gmail.com>
2017-05-22 13:47:10 +1200

    Update JS, button will change text (show/hide) depending on state

--- public/js/hideShow.js
+++ public/js/hideShow.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
+
+    $('.show-hide-js').each(function(){
+       	var $dis = $(this),
+    		originalbuttontext= $dis.text();
+
+		$dis.attr('data-original-value', originalbuttontext);
+    });
+
     $('.show-hide-js').each(function(index, el) {
-        $(this).on('click', function() {
+		var $dis = $(this);
+
+        $dis.on('click', function() {
+
+        	if($dis.hasClass('open')){
+	        	$dis.removeClass('open');
+        		$dis.html($dis.attr('data-original-value'));
+        	}else {
+	        	$dis.addClass('open');
+        		$dis.html($dis.attr('data-alt-value'));
+        	}
+
             $(this).next().toggle();
         });
+
     });
 });
