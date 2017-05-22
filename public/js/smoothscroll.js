0f38fa2df7a71aa9604c5426bfa8e3684cf6dc71
Bas van Lunteren <bas.van.lunteren@gmail.com>
2017-05-22 13:57:34 +1200

    Add scroll range for first nav element (it includes heading space), update URL when using lhs nav

--- public/js/smoothscroll.js
+++ public/js/smoothscroll.js
@@ -1,14 +1,47 @@
 $(document).ready(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
-    }
-  });
+
+	var HeaderHeight = $('header').outerHeight() + 40;
+
+	if (window.location.hash.length) {
+		var target = $(window.location.hash),
+		level = target.attr('id').split('-').length;
+
+		smoothscroll(target, level);
+	}
+
+	$('a[href*="#"]:not([href="#"])').click(function() {
+
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash),
+				level;
+
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			//level will check if the element clicked is the main level, which means it needs to scroll up further to show the hearder
+			level = target.attr('id').split('-').length;
+
+			smoothscroll(target, level)
+
+			return false;
+		}
+	});
+
+	function smoothscroll(target, level){
+
+		if (target.length) {
+
+			if(level <= 2) {
+				$('html, body').animate({
+					scrollTop: 0
+				}, 500);
+			}else{
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 500);
+			}
+
+			window.location = window.location.pathname + '#' + target.attr('id');
+
+		};
+	};
+
 });
