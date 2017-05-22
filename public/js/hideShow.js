1fb65bd75e10bbd393f571514bb50486881d2b61
Bas van Lunteren <bas.van.lunteren@gmail.com>
2017-05-22 13:51:52 +1200

    Add show hide functionality for large sections within ‘.main’

--- public/js/hideShow.js
+++ public/js/hideShow.js
@@ -24,4 +24,40 @@ $(document).ready(function() {
         });
 
     });
+
+    // add a show/hide button to all sections within .main
+
+    $('.kss-section').each(function(){
+        var $dis = $(this),
+        $firstheading = $dis.find('h2:first');
+
+        if($('.show-hide-content').length){
+            $('<button class="show_hide-content-button" value="+"/>').insertAfter($firstheading);
+        }
+    });
+
+
+    // show hide content
+
+    $('.kss-section .show_hide-content-button').on('click', function(e){
+        var $dis = $(this),
+        $dissection = $dis.parents('.kss-section:first'),
+        $container = $dissection.find('.show-hide-content:first'),
+        containerinitialheight;
+
+
+        if($dissection.hasClass('open')){
+            containerinitialheight = $container.height();
+
+            $container.attr('data-height', containerinitialheight);
+
+            $dissection.removeClass('open');
+            $container.css({'height': '0px'});
+        }else{
+            $dissection.addClass('open');
+            $container.css({'height': 'auto'})
+        }
+
+        e.preventDefault();
+    });
 });
