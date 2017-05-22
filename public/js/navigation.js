590907dcbc1b90e80afe85916754aae93d78696e
Bas van Lunteren <bas.van.lunteren@gmail.com>
2017-05-22 13:52:26 +1200

    Add show/hide functionality for sub navigation

---
+++ public/js/navigation.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+
+    // add a show/hide button to all nav levels that have sub navigation
+
+    $('.kss-nav__menu-item').each(function(){
+        var $dis = $(this);
+
+        if($dis.find('ul').length){
+            $dis.addClass('hassubnav');
+            $('<button class="show_hide-content-button" value="+"/>').insertAfter($dis.find('a:first'));
+        }
+    });
+
+
+    // show hide subnavigation
+
+    $('.kss-nav .show_hide-content-button').on('click', function(e){
+        var $dis = $(this),
+        $aparently = $dis.parents('li:first');
+
+        $aparently.toggleClass('open');
+
+        e.preventDefault();
+    });
+});
