beb18c3d1229f391b3a4c5af2d26345e54688242
Bas van Lunteren <bas.van.lunteren@chrometoaster.com>
2017-06-13 19:13:24 +1200

    Update script

--- public/js/smoothscroll.js
+++ public/js/smoothscroll.js
@@ -1,46 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	var HeaderHeight = $('header').outerHeight() + 40;
+    var HeaderHeight = $('header').outerHeight() + 40;
 
-	if (window.location.hash.length) {
-		var target = $(window.location.hash),
-		level = target.attr('id').split('-').length;
+    if (window.location.hash.length) {
+        var target = $(window.location.hash),
+        level = target.attr('id').split('-').length;
 
-		smoothscroll(target, level);
-	}
+        smoothscroll(target, level);
+    }
 
-	$('a[href*="#"]:not([href="#"])').click(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
 
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash),
-				level;
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash),
+            level;
 
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			//level will check if the element clicked is the main level, which means it needs to scroll up further to show the hearder
-			level = target.attr('id').split('-').length;
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            //level will check if the element clicked is the main level, which means it needs to scroll up further to show the hearder
+            level = target.attr('id').split('-').length;
 
-			smoothscroll(target, level)
+            smoothscroll(target, level)
 
-			return false;
-		}
-	});
+            return false;
+        }
+    });
 
-	function smoothscroll(target, level){
+    function smoothscroll(target, level){
 
-		if (target.length) {
+        if (target.length) {
 
-			if(level <= 2) {
-				$('html, body').animate({
-					scrollTop: 0
-				}, 500);
-			}else{
-				$('html, body').animate({
-					scrollTop: target.offset().top
-				}, 500);
-			}
+            if(level <= 2) {
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 500);
+            }else{
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+            }
 
-			window.location = window.location.pathname + '#' + target.attr('id');
+            window.location = window.location.pathname + '#' + target.attr('id');
 
-		};
-	};
+        };
+    };
 });
